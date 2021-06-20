9e9f30898ba9b6f51b74c64a53252fbc2203c652
f.lorenzo <f.lorenzo@reply.it>
2021-06-20 13:14:10 +0200

    Made toast dynamic

--- src/components/UI/Toast/Toast.tsx
+++ src/components/UI/Toast/Toast.tsx
@@ -1,35 +1,96 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 import styles from "./Toast.module.scss";
 
+/*
+ * Define a given element for the toast component
+ */
+interface ToastElement {
+    id: number;
+    title: string;
+    description: string;
+    backgroundColor: string;
+    icon: 'success' | 'error' | 'info' | 'warning';
+    /**
+     * Icon image, added automatically based on the 'icon' property
+     */
+    iconPng?: any;
+    /**
+     * Icon alternative text, added automatically based on the 'icon' property
+     */
+    iconDesc?: string;
+}
+
 interface ToastProps {
     /**
-     * Array that contains objects...
+     * List of all the messages that should be printed with a Toast (one object per Toast), with all related properties
      */
-    toastList?: Array<any>;
+    toastList: Array<ToastElement>;
     /**
      * Placement of the notification container on the page
      */
-    position?: string;
+    position: 'Top' | 'Bottom' | 'TopRight' | 'TopLeft' | 'BottomRight' | 'BottomLeft';
 }
 
-const toast = (props: ToastProps) => {
+const Toast = (props: ToastProps) => {
+    const [list, setList] = useState<Array<ToastElement>>(props.toastList);
+
+    useEffect(() => {
+        setList(props.toastList.map((toast) => {
+            let iconPng, iconDesc;
+
+            switch (toast.icon) {
+                case 'success':
+                    iconPng= 'X'
+                    iconDesc= 'success message icon'
+                    break;
+                case 'error':
+                    iconPng= 'X'
+                    iconDesc= 'error message icon'
+                    break;
+                case 'info':
+                    iconPng= 'X'
+                    iconDesc= 'info message icon'
+                    break;
+                case 'warning':
+                    iconPng= 'X'
+                    iconDesc= 'warning message icon'
+                    break;
+            }
+            return {
+                ...toast,
+                iconPng: iconPng,
+                iconDesc: iconDesc
+            }
+        }));
+    }, [props.toastList, list]);
+
     return (
         <>
-            <div className={styles.NotificationContainer}>
-                <div className={[styles.Notification, styles.Toast].join(" ")}>
-                    <button>X</button>
-                    <div className={styles.NotificationImage}>
-                        <img src="" alt="" />
-                    </div>
-                    <div>
-                        <p className={styles.NotificationTitle}>Title</p>
-                        <p className={styles.NotificationMessage}>Message</p>
-                    </div>
-                </div>
+            <div className={[styles.NotificationContainer, styles[props.position]].join(" ")}>
+                {
+                    list.map((toast, i) => (
+                        <div 
+                            key={i} 
+                            className={[styles.Notification, styles.Toast, styles[props.position]].join(" ")}
+                            style={{ backgroundColor: toast.backgroundColor }}
+                         >
+                            <button>X</button>
+                            <div className={styles.NotificationImage}>
+                                <img src={toast.iconPng} alt={toast.iconDesc} />
+                            </div>
+                            <div>
+                                <p className={styles.NotificationTitle}>{toast.title}</p>
+                                <p className={styles.NotificationMessage}>{toast.description}</p>
+                            </div>
+                        </div>
+                        )
+                    )
+                }
+                
             </div>
         </>
     );
 };
 
-export default toast;
+export default Toast;
