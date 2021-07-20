3d97a548d03ead7b4b47b744231a73a2e4874b55
f.lorenzo <f.lorenzo@reply.it>
2021-07-20 08:07:28 +0200

    Introduced Google Sign In

--- src/containers/Authentication/Login/VolunteerStandardLogin/VolunteerStandardLogin.tsx
+++ src/containers/Authentication/Login/VolunteerStandardLogin/VolunteerStandardLogin.tsx
@@ -1,87 +1,64 @@
 import { Link } from "react-router-dom";
 
+import useVolunteerStandardLogin from "./useVolunteerStandardLogin";
+
 import Button from "../../../../components/UI/Button/Button";
-// import useLogin from "./hooks/useLogin"
 
 import * as translations from "../../Authentication.translations";
 import styles from "../../Authentication.module.scss";
-
-import { useHistory } from "react-router";
 
 import facebookLogo from "../../../../assets/images/facebookLogo.png";
 import googleLogo from "../../../../assets/images/googleLogo.svg";
 
 interface StandardLoginProps {
-    emailPageTextClicked(): void;
+	emailPageTextClicked(): void;
 }
 
 export const VolunteerStandardLogin = (props: StandardLoginProps) => {
-    let history = useHistory();
+	const {
+		onFacebookLoginClicked,
+		onGoogleLoginClicked,
+		onEmailPageButtonClicked,
+	} = useVolunteerStandardLogin();
 
-    // Act upon detecting the click of the search bar
-    const onEmailPageButtonClicked = (e: any) => {
-        e.preventDefault();
+	return (
+		<>
+			<div className={styles.LoginSection}>
+				<div className={styles.TitleGroup}>
+					<h1 className={styles.Title}>{translations.loginAccessWith}</h1>
+				</div>
+				<div className={styles.ButtonGroup}>
+					<div className={styles.Button}>
+						<Button colorStyle="White" clicked={onGoogleLoginClicked}>
+							<img src={googleLogo} alt="Google logo" />
+							Google
+						</Button>
+					</div>
+					<div className={styles.Button}>
+						<Button colorStyle="White" clicked={onFacebookLoginClicked}>
+							<img src={facebookLogo} alt="Facebook logo" />
+							Facebook
+						</Button>
+					</div>
+				</div>
+			</div>
 
-        history.push({
-            pathname: "/login/volunteer/email",
-        });
-    };
+			<div className={styles.LoginSection}>
+				<div className={styles.Text}>{translations.loginInstead}</div>
+				<div className={styles.ButtonGroup}>
+					<div className={styles.Button}>
+						<Button colorStyle="White" clicked={onEmailPageButtonClicked}>
+							{translations.loginContinueWithEmail}
+						</Button>
+					</div>
+				</div>
+			</div>
 
-    return (
-        <>
-            <div className={styles.LoginSection}>
-                <div className={styles.TitleGroup}>
-                    <h1 className={styles.Title}>
-                        {translations.loginAccessWith}
-                    </h1>
-                </div>
-                <div className={styles.ButtonGroup}>
-                    <div className={styles.Button}>
-                        <Button
-                            colorStyle="White"
-                            clicked={() => {
-                                console.log("Clicked");
-                            }}
-                        >
-                            <img src={googleLogo} alt="Google logo" />
-                            Google
-                        </Button>
-                    </div>
-                    <div className={styles.Button}>
-                        <Button
-                            colorStyle="White"
-                            clicked={() => {
-                                console.log("Clicked");
-                            }}
-                        >
-                            <img src={facebookLogo} alt="Facebook logo" />
-                            Facebook
-                        </Button>
-                    </div>
-                </div>
-            </div>
-
-            <div className={styles.LoginSection}>
-                <div className={styles.Text}>{translations.loginInstead}</div>
-                <div className={styles.ButtonGroup}>
-                    <div className={styles.Button}>
-                        <Button
-                            colorStyle="White"
-                            clicked={onEmailPageButtonClicked}
-                        >
-                            {translations.loginContinueWithEmail}
-                        </Button>
-                    </div>
-                </div>
-            </div>
-
-            <div className={styles.LoginSection}>
-                <div className={styles.LinkText}>
-                    <Link to="/login/organization">
-                        {translations.loginOrganization}
-                    </Link>
-                </div>
-            </div>
-        </>
-    );
+			<div className={styles.LoginSection}>
+				<div className={styles.LinkText}>
+					<Link to="/login/organization">{translations.loginOrganization}</Link>
+				</div>
+			</div>
+		</>
+	);
 };
--- src/containers/Authentication/Login/VolunteerStandardLogin/useVolunteerStandardLogin.tsx
+++ src/containers/Authentication/Login/VolunteerStandardLogin/useVolunteerStandardLogin.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react";
+import { useState } from "react";
 
 import { useHistory } from "react-router-dom";
 
@@ -9,75 +9,89 @@ import useToast from "../../../../components/UI/Toast/useToast";
 /**
  * Business logic for the VolunteerSignUp component
  */
-// TODO: Fix to adapt to the standard login
 export default function useVolunteerStandardLogin() {
-    const emailRef = useRef<HTMLInputElement>(null);
-    const passwordRef = useRef<HTMLInputElement>(null);
-    const { loginWithEmailAndPassword } = useAuth();
-    const [loading, setLoading] = useState(false);
+	const { loginWithFacebook, loginWithGoogle } = useAuth();
+	const [loading, setLoading] = useState(false);
 
-    const history = useHistory();
+	const history = useHistory();
 
-    const { setToastErrorMessage } = useToast();
+	const { setToastErrorMessage } = useToast();
 
-    /**
-     * Function to be triggered when user clicks the login button. First performs a validation check, then
-     * triggers the user login process. If everything goes well, redirects the user to the home page
-     */
-    const handleSubmit = async (e: any) => {
-        e.preventDefault();
+	/**
+	 * When user clicks the FB Login button, trigger the login with Facebook and handle messages accordingly
+	 */
+	// TODO: Add translations
+	const onFacebookLoginClicked = (e: any) => {
+		e.preventDefault();
 
-        if (!loginWithEmailAndPassword) return;
+		if (!loginWithFacebook) return;
 
-        // TODO: Set up a proper form field verification to check nothing is wrong
-        // TODO: Substitute with translations
-        if (
-            emailRef?.current?.checkValidity() &&
-            passwordRef?.current?.checkValidity()
-        ) {
-            try {
-                setLoading(true);
+		try {
+			setLoading(true);
 
-                loginWithEmailAndPassword(
-                    emailRef.current.value,
-                    passwordRef.current.value
-                )
-                    .then(() => {
-                        history.push("/");
-                    })
-                    .catch((error: any) => {
-                        setToastErrorMessage("Ooops", error);
-                    });
-            } catch (error) {
-                setToastErrorMessage(
-                    "Errore",
-                    "Si è verificato un errore durante il login. Si prega di riprovare più tardi."
-                );
-            }
+			loginWithFacebook()
+				.then(() => {
+					history.push("/");
+				})
+				.catch((error: any) => {
+					setToastErrorMessage("Ooops", error);
+				});
+		} catch (error) {
+			setToastErrorMessage(
+				"Errore",
+				"Si è verificato un errore durante il login. Si prega di riprovare più tardi."
+			);
+		}
 
-            setLoading(false);
-        } else {
-            setToastErrorMessage(
-                "Dati mancanti",
-                "Completa tutti i campi per poter accedere"
-            );
-        }
-    };
+		setLoading(false);
+	};
 
-    // Act upon detecting the click of the search bar
-    const onEmailPageButtonClicked = (e: any) => {
-        e.preventDefault();
+	/**
+	 * When user clicks the Google Login button, trigger the login with Google and handle messages accordingly
+	 */
+	// TODO: Add translations
+	const onGoogleLoginClicked = (e: any) => {
+		e.preventDefault();
 
-        history.push({
-            pathname: "/login/volunteer/email",
-        });
-    };
+		if (!loginWithGoogle) return;
 
-    return {
-        emailRef,
-        passwordRef,
-        loading,
-        handleSubmit,
-        onEmailPageButtonClicked,
-    };
+		try {
+			setLoading(true);
+
+			loginWithGoogle()
+				.then(() => {
+					history.push("/");
+				})
+				.catch((error: any) => {
+					console.log(error);
+					setToastErrorMessage("Ooops", error);
+				});
+		} catch (error) {
+			console.log(error);
+			setToastErrorMessage(
+				"Errore",
+				"Si è verificato un errore durante il login. Si prega di riprovare più tardi."
+			);
+		}
+
+		setLoading(false);
+	};
+
+	/**
+	 * Act upon detecting the click of the link on the "Login with email button"
+	 */
+	const onEmailPageButtonClicked = (e: any) => {
+		e.preventDefault();
+
+		history.push({
+			pathname: "/login/volunteer/email",
+		});
+	};
+
+	return {
+		loading,
+		onFacebookLoginClicked,
+		onGoogleLoginClicked,
+		onEmailPageButtonClicked,
+	};
 }
